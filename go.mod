c50bd32095b816a17cdfb0eb954bd91a1a586c39
Guilherme <guiteixeira1926@gmail.com>
2026-02-09 16:58:48 -0300

    feat(producer): add basic producer

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/Guizzs26/go-sync-db
 
 go 1.25.0
+
+require github.com/rabbitmq/amqp091-go v1.10.0
---
+++ main.go
@@ -0,0 +1,81 @@
+package main
+
+import (
+	"context"
+	"encoding/json"
+	"log"
+	"time"
+
+	amqp "github.com/rabbitmq/amqp091-go"
+)
+
+type Order struct {
+	ID        string  `json:"id"`
+	Customer  string  `json:"customer"`
+	Amount    float64 `json:"amount"`
+	Status    string  `json:"status"`
+	Timestamp string  `json:"timestamp"`
+}
+
+func main() {
+	conn, err := amqp.Dial("amqp://guest:guest@localhost:5672/")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	ch, err := conn.Channel()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	if err = ch.Confirm(false); err != nil {
+		log.Fatal(err)
+	}
+
+	confirms := ch.NotifyPublish(make(chan amqp.Confirmation, 1))
+
+	if err = ch.ExchangeDeclare(
+		"orders.v1.topic",
+		"topic",
+		true,
+		false,
+		false,
+		false,
+		nil,
+	); err != nil {
+		log.Fatal(err)
+	}
+
+	order := Order{
+		ID:        "PED-12345",
+		Customer:  "Pax Primavera",
+		Amount:    2500.75,
+		Status:    "pending",
+		Timestamp: time.Now().Format(time.RFC3339),
+	}
+	body, _ := json.Marshal(order)
+
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	if err := ch.PublishWithContext(
+		ctx,
+		"orders.v1.topic",
+		"order.created",
+		false,
+		false,
+		amqp.Publishing{
+			DeliveryMode: amqp.Persistent,
+			ContentType:  "application/json",
+			Body:         []byte(body),
+		},
+	); err != nil {
+		log.Fatal(err)
+	}
+
+	if confirmed := <-confirms; confirmed.Ack {
+		log.Printf(" [OK] Pedido %s confirmado pelo Broker no disco!", order.ID)
+	} else {
+		log.Printf(" [ERRO] Mensagem %s não foi confirmada (Nack do Broker)", order.ID)
+	}
+}
